d108ef00dc10b8f78354ec04cdff79c87525e12a
Matveismi <matvej150@gmail.com>
2024-04-07 12:32:56 +0300

    working chat

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = 3000;
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"index.html"));
+    res.sendFile(path.join(__dirname,"public","index.html"));
+});
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname,"public",req.path));
 });
 
 server.listen(port, () => {
@@ -20,7 +23,12 @@ server.listen(port, () => {
 
 io.on('connection', (socket)=>{
     console.log("Пользователь +");
+    
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
     socket.on('disconnect', () => {
         console.log('Пользователь -');
     });
+
 });
