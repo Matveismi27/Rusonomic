677e5c5bc37c6876022986fe7f7467e3e1c9a82d
Matveismi <matvej150@gmail.com>
2024-05-07 23:21:36 +0300

    registration

--- authController.js
+++ authController.js
@@ -1,13 +1,34 @@
 const pool = require("./db");
-
-
+const crypto = require('node:crypto');
+require("dotenv").config(); 
+const jwt = require("jsonwebtoken"); 
 class authController{
     async registration(req, res){
         console.log("registration");
         try {
             console.log(req.body);
             console.log(req.body.password);
+            if (req.body.password == req.body.confirm_password && req.body.username && req.body.password){
+                let password = crypto
+                        .createHmac("sha256", process.env.HASH_SECRET)
+                        .update(req.body.password)
+                        .digest("base64");
+                let query = `
+                INSERT INTO public.players
+                ("name", "level", balance, "date", "password", login)
+                VALUES('${req.body.username}', 0, 0, CURRENT_TIMESTAMP, '${password}', '${req.body.username}');
+                `;
+                await pool.query(query);
+                res.redirect("/login");
+
+            }else{
+                res.send("Проверьте правильность введённых данных");
+            }
+            
         } catch (error) {
+            if (error.constraint == "players_unique"){
+                await res.redirect("/registration");
+            }
             console.log(error);
         }
     }
@@ -15,7 +36,42 @@ class authController{
     async login(req, res){
         console.log("login");
         try {
-            console.log(req);
+            try {
+                let query = `
+                    SELECT login, password
+                    FROM public.players;
+                `;
+                let result = await pool.query(query);
+                console.log(result.rows);
+                console.log(req.body);
+                let rows = result.rows
+                for (let row of rows){
+                    if (row.login == req.body.username){
+                        const username = req.body.username;
+                        const hash = crypto
+                            .createHmac("sha256", process.env.HASH_SECRET)
+                            .update(req.body.password)
+                            .digest("base64");
+                        if( row.password == hash){
+                            // Удачно
+                            console.log("Удачная авторизация");
+                            const token = jwt.sign({ username },  
+                            process.env.JWT_SECRET_KEY, { 
+                                expiresIn: 86400 
+                            }); 
+                            req.body.token = token;
+                            return res.json({ username, token, msg: "Login Success" });
+                        }else{
+                            console.log("Неудачная попытка авторизоваться");
+                            return res.res.json({msg:"Пароли не совпадают"});
+                        }
+                    }
+                }
+                console.log("Пользователь не найден");
+                return res.json({msg: "Пользователь не найден"});
+            } catch (error) {
+                console.log(error);
+            }
         } catch (error) {
             console.log(error);
         }
@@ -23,16 +79,7 @@ class authController{
 
     async getUsers(req, res){
         console.log("getUsers");
-        try {
-            let query = `
-            SELECT id, "name", "level", balance, "date", login
-            FROM public.players;
-            `;
-            let result = await pool.query(query);
-            res.json(result.rows);
-        } catch (error) {
-            console.log(error);
-        }
+        
     }
 }
 module.exports = new authController();
--- routes.js
+++ routes.js
@@ -4,26 +4,38 @@ const path = require('path');
 const pool = require("./db");
 const multer = require("multer");
 const upload = multer();
+const urlencodedParser = express.urlencoded({extended: false});
+require("dotenv").config(); 
+const jwt = require("jsonwebtoken"); 
+const cookieParser = require("cookie-parser");
 
 const { createServer } = require("http");
 const authController = require('./authController');
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static('public'));
 app.get("/", (req, res) => {
-    if (!req.body.token) {
-        console.log(req.body);
-        res.redirect("/login");
+    if (!req.cookies.token) {
+        return res.redirect("/login");
+    }
+    const token = req.cookies.token;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); 
+        req.user = decoded; 
+    } catch (error) {
+        console.log(error);
+        return res.redirect("/login"); 
     }
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/login',authController.login)
+app.post('/login',urlencodedParser,authController.login)
 app.get('/login',(req,res)=>{
     res.sendFile(path.join(__dirname,"public" ,"login.html"));
 })
-app.post('/registration',authController.registration)
-app.get('/registration', upload.none(),(req,res)=>{
+app.post('/registration',urlencodedParser,authController.registration)
+app.get('/registration',(req,res)=>{
     res.sendFile(path.join(__dirname,"public" ,"register.html"));
 })
 app.post('/users',authController.getUsers)
